7b7567f093eea43021f0e3706f98e828013973ad
LaylaEBT <136167430+LaylaEBT@users.noreply.github.com>
2025-06-10 08:03:27 +0200

    addUIscript

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Dragon1"); // Replace with your game scene name
+    }
+
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void BackToMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+}
+
